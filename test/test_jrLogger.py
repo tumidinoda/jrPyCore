6e1131730140207e112cf1740e728b997e61fb89
robert@Master-PC <robert.jonas@gmx.at>
2017-01-23 21:57:03 +0100

    continue with logging

--- test/test_jrLogger.py
+++ test/test_jrLogger.py
@@ -1,32 +1,39 @@
-from os import environ
 import logging
 import unittest
+from os import environ
 from unittest import TestCase
 
 import jrLogger
 
+# initialize logging
+jrLogger.config()
+myLogger = logging.getLogger(__name__)
+
 
 # ---------------------------------------------------------------------------------------------------------
 class TestJrLogger(TestCase):
-    myLogger=jrLogger.JrLogger()
 
-    #logging based on default log-options
+    # logging based on default log-options
+    environ['LOG_CFG'] = ''
+    jrLogger.config()
     logging.debug('Hallo Debug')
     logging.info('Hallo Info')
-    logging.warning('Hallo Warning')
-    logging.error('Hallo Error')
-    logging.critical('Hallo Critical')
-    logging.exception('Hallo Error with traceback')
+    myLogger.warning('Hallo Warning')
+    myLogger.error('Hallo Error')
+    myLogger.critical('Hallo Critical')
+    myLogger.exception('Hallo Error with traceback')
 
-    #logging based on config file options
-    environ['LOG_CFG']='../jrLogger.yaml'
-    myLogger.config()
-    logging.debug('Hallo Debug')
-    logging.info('Hallo Info')
-    logging.warning('Hallo Warning')
-    logging.error('Hallo Error')
-    logging.critical('Hallo Critical')
-    logging.exception('Hallo Error with traceback')
+
+    # logging based on config file options
+    environ['LOG_CFG'] = '../jrLogger.yaml'
+    jrLogger.config()
+    myLogger.debug('Hallo Debug')
+    myLogger.info('Hallo Info')
+    myLogger.warning('Hallo Warning')
+    myLogger.error('Hallo Error')
+    myLogger.critical('Hallo Critical')
+    myLogger.exception('Hallo Error with traceback')
+
 
 # ---------------------------------------------------------------------------------------------------------
 __name__ == '__main__' and unittest.main()
