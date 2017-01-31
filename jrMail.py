9d7a920578691a3c08213907e662b7e28f84d878
robert@gitkraken <robert.jonas@gmx.at>
2017-01-31 17:01:42 +0100

    mail class improvements

--- jrMail.py
+++ jrMail.py
@@ -2,17 +2,22 @@ import netrc
 import smtplib
 from email.mime.text import MIMEText
 
-from jrPyCore.jrLogger import JrLogger
+from jrLogger import JrLogger
 
 
 class JrMail:
     # -----------------------------------------------------------------------------------------------------
-    def __init__(self, netrc_key='Mailprovider'):
-        self.__mail_from = 'WetterSeyring@a1.net'
-        self.__mail_to = 'robert.jonas@gmx.at'
-        secrets = netrc.netrc('.myNetrc')
+    def __init__(self, netrc_path='.jrNetrc', netrc_key='Mailprovider', mail_from='WetterSeyring@a1.net',
+                 mail_to='robert.jonas@gmx.at'):
+        self.__mail_from = mail_from
+        self.__mail_to = mail_to
+        try:  # first default .netrc and second local
+            secrets = netrc.netrc()
+        except (netrc.NetrcParseError, OSError):
+            secrets = netrc.netrc(netrc_path)
+
         self.__mail_user, self.__smtp_server, self.__mail_pw = secrets.authenticators(netrc_key)
-        self.logger = JrLogger().config(__name__)
+        self.__logger = JrLogger().config(__name__)
 
     # -----------------------------------------------------------------------------------------------------
     def send(self, subject, content=None):
@@ -26,17 +31,23 @@ class JrMail:
         server.login(self.__mail_user, self.__mail_pw)
         server.sendmail(self.__mail_from, self.__mail_to, msg.as_string())
         server.quit()
-        self.logger.info('Mail to ' + self.__mail_to + ' sent')
+        self.__logger.info('Mail to ' + self.__mail_to + ' sent')
 
 
 # ---------------------------------------------------------------------------------------------------------
 def main():
-    # test subject only mail
-    my_mail = JrMail()
-    my_mail.send('Testsubject')
-    # test subject and content mail
-    my_mail.send("Testsubject 2", "Testcontent")
-    my_mail.logger.info('JRmail() Tests finished')
+    my_logger = JrLogger().config(__file__)
+    try:
+        my_mail = JrMail()
+        my_logger.info('JRmail() Tests started')
+        # test subject only mail
+        my_mail.send('Testsubject')
+        # test subject and content mail
+        my_mail.send("Testsubject 2", "Testcontent")
+        my_logger.info('JRmail() Tests finished')
+    except:
+        my_logger.exception('Unknown Error')
+        raise
 
 
 # ---------------------------------------------------------------------------------------------------------
