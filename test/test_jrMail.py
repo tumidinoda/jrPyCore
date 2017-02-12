2be55dc581212a957c85b2c6cda7a366c0877479
robert@gitkraken <robert.jonas@gmx.at>
2017-02-12 12:47:25 +0100

    unittest mock object introduced

--- test/test_jrMail.py
+++ test/test_jrMail.py
@@ -1,11 +1,13 @@
+from unittest.mock import Mock
 from jrPyCore.jrLogger import JrLogger
 from jrPyCore.jrMail import JrMail
 
-my_logger = JrLogger(__name__, file='../jrLogger.yml').get()
+my_logger = JrLogger(__name__, conffile='../jrLogger.yml').get()
 try:
     myMail = JrMail('../.jrNetrc')
+    myMail.server = Mock()
+
     myMail.send('Test', 'JrMail Test von Testmodule')
 except:
     my_logger.exception('Unknown Error')
-    raise
-    print('Hallo ')
+    raise
